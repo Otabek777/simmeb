35436090e0d58d119757d53ea5e009c5602c92e5
Otabek777 <53979293+Otabek777@users.noreply.github.com>
2023-07-11 08:36:03 +0500

    tab

--- js/main.js
+++ js/main.js
@@ -19,3 +19,26 @@ $(".filter__mobileBtnOpen").click(function() {
 $(".filter__mobileBtnClose").click(function() {
     $(".filter").removeClass("open");
 });
+
+// Tab
+if (window.innerWidth < 1024) {
+    $(".product__tabBtn .btn").click(function() {
+        if($(this).hasClass("active")) {
+            $(".product__tabBtn").addClass("open");
+        } else {
+            $(".product__tabBtn").removeClass("open");
+        };
+    });
+};
+function TabClick(num) {
+    $("#tabBtn-"+num).click(function() {
+        $(".product__tabBtn .btn").removeClass("active");
+        $(".product__tabBlock").removeClass("active");
+        $(this).addClass("active");
+        $("#tabBlock-"+num).addClass("active");
+    });
+};
+TabClick("1");
+TabClick("2");
+TabClick("3");
+TabClick("4");
